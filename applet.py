b59485724a3f14b68f8ce4f37ff6d4f2eb87579d
defseg <6988161+defseg@users.noreply.github.com>
2018-05-02 11:19:11 -0400

    Initialize

---
+++ applet.py
@@ -0,0 +1,28 @@
+from gi.repository import Gtk
+from gi.repository import AppIndicator3
+import signal
+
+APPINDICATOR_ID = "bostoncalendar"
+
+def main():
+  indicator = AppIndicator3.Indicator.new(APPINDICATOR_ID, "asdf", AppIndicator3.IndicatorCategory.SYSTEM_SERVICES)
+  indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
+  indicator.set_menu(build_menu())
+  # Make it possible to ctrl+c
+  signal.signal(signal.SIGINT, signal.SIG_DFL)
+  # Start GTK main loop
+  Gtk.main()
+
+def build_menu():
+  menu = Gtk.Menu()
+  item_quit = Gtk.MenuItem("Quit")
+  item_quit.connect("activate", quit)
+  menu.append(item_quit)
+  menu.show_all()
+  return menu
+
+def quit(event_source):
+  Gtk.main_quit()
+
+if __name__ == "__main__":
+  main()
