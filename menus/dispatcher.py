74510c2bd713fc290e589bd9516e778cc98ba927
defseg <6988161+defseg@users.noreply.github.com>
2018-08-15 01:59:08 -0400

    Start rewriting to use QT

---
+++ menus/dispatcher.py
@@ -0,0 +1,36 @@
+from webbrowser import open as open_in_browser
+from PySide.QtGui import qApp
+
+class Dispatcher:
+    '''Dispatcher singleton so we can pass in methods from the applet without
+    having to keep passing dictionaries into QtMenu.
+    	The dispatcher is initialized with a dictionary of functions, to which
+    some default functions are appended. All these functions take one argument:
+    the menu item to which they're associated.'''
+    __instance = None
+
+    def __init__(self, funcs):
+    	if Dispatcher.__instance == None:
+    		Dispatcher.__instance = self
+    		self.__funcs = {}
+    		self.__funcs.update(funcs)
+    		self.__funcs.update({
+    			'navigate': _navigate,
+    			'quit':     _quit
+    		})
+
+    @staticmethod 
+    def get(obj):
+        return _curry(Dispatcher.__instance.__funcs[obj.action], obj)
+
+def _curry(func, arg):
+	return lambda *a, **b: func(arg, *a, **b)
+
+def _navigate(obj):
+	if obj.data and obj.data.get('url'):
+		open_in_browser(obj.data['url'])
+
+def _quit(obj):
+	qApp.quit()
+
+Dispatcher({})
--- menus/indicator.py
+++ menus/indicator.py
@@ -1,14 +1,58 @@
-'''Import the Indicator module appropriate for the system.'''
+from PySide import QtGui
+from dispatcher import Dispatcher
+from xml_to_obj import MenuItem
+import sys
 
-import platform
-system = platform.system()
+class SystemTrayIcon(QtGui.QSystemTrayIcon):
+    def __init__(self, icon, parent=None):
+        QtGui.QSystemTrayIcon.__init__(self, icon, parent)
+        self.menu = BaseMenu()
+        self.setContextMenu(self.menu)
 
-if system == 'Windows':
-	from windows.indicator import Indicator
-elif system == 'Linux':
-	from gtk.indicator import Indicator
-else:
-	raise IndicatorError('Unsupported OS: {}'.format(system))
+    def add_needed_items(self):
+        quit_option = self.menu.addAction('Quit')
+        quit_option.triggered.connect(QtGui.qApp.quit)
+
+class BaseMenu(QtGui.QMenu):
+    def __init__(self):
+        QtGui.QMenu.__init__(self, "")
+        self.widgets = {}
+
+    def add_widget(self, menu):
+        assert menu.__class__.__name__ == "Menu"
+        self.widgets[menu.text] = QtMenu(menu, parent=self)
+        self.addMenu(self.widgets[menu.text])
+
+class QtMenu(QtGui.QMenu):
+    def __init__(self, menu, parent=None):
+        QtGui.QMenu.__init__(self, menu.text, parent)
+        for i in menu.items:
+            self.__add(i)
+
+    def __add(self, thing):
+        if thing.__class__.__name__ == "Menu":
+            self.addMenu(QtMenu(thing, parent=self))
+        elif thing.__class__.__name__ == "MenuItem":
+            menu_item = self.addAction(thing.text)
+            if thing.action:
+                menu_item.triggered.connect(Dispatcher.get(thing))
+        else:
+            raise IndicatorError("{} isn't menu or item".format(thing))
+
+class Indicator():
+    def __init__(self):
+        self.app     = QtGui.QApplication(sys.argv)
+        icon_graphic = QtGui.QApplication.style().standardIcon(QtGui.QStyle.SP_DriveDVDIcon)
+        self.icon    = SystemTrayIcon(icon_graphic)
+
+    def set_menu(self, menu_base):
+        for w in menu_base.items:
+            self.icon.menu.add_widget(w)
+        self.icon.add_needed_items()
+
+    def go(self):
+        self.icon.show()
+        sys.exit(self.app.exec_())
 
 class IndicatorError(Exception):
-	pass
+    pass
